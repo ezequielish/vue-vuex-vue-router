c6c02e1ffa4d16b13309b10107c9c1942b1854e9
ezequielish <ezequielprophetic@gmail.com>
2020-04-13 12:52:12 -0400

    methods

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Methods msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Methods from './components/Methods.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Methods
   }
 }
 </script>
